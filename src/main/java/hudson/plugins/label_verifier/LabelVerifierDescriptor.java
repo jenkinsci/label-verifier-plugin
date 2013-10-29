8729bbfa31fbd0ac4e5165b711a1574e9416d695
Oleg Nenashev <nenashev@synopsys.com>
2013-10-29 12:48:12 +0400

    Added draft config.jelly files
    Signed-off-by: Oleg Nenashev <nenashev@synopsys.com>

--- src/main/java/hudson/plugins/label_verifier/LabelVerifierDescriptor.java
+++ src/main/java/hudson/plugins/label_verifier/LabelVerifierDescriptor.java
@@ -4,6 +4,7 @@ import hudson.DescriptorExtensionList;
 import hudson.matrix.AxisDescriptor;
 import hudson.model.Descriptor;
 import hudson.model.Hudson;
+import java.util.List;
 
 /**
  * @author Kohsuke Kawaguchi
@@ -14,5 +15,16 @@ public abstract class LabelVerifierDescriptor extends Descriptor<LabelVerifier> 
      */
     public static DescriptorExtensionList<LabelVerifier,LabelVerifierDescriptor> all() {
         return Hudson.getInstance().getDescriptorList(LabelVerifier.class);
+    }
+    
+    /**
+     * Gets available descriptors.
+     * Function is dedicated to be used in logic expressions and other verifiers,
+     * which contain nested descriptors.
+     * @return all the registered {@link LabelVerifierDescriptor}s.
+     * @since 1.1
+     */
+    public List<LabelVerifierDescriptor> getVerifierDescriptors() {
+        return LabelVerifierDescriptor.all();
     }
 }
